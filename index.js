415f6e6a1585311e5404aaa8f4ce15635fca722d
Marcelo <marcelorubenayala@outlook.com>
2023-08-30 15:19:23 -0300

    Adding new features

--- index.js
+++ index.js
@@ -1,20 +1,61 @@
-const express = require('express')
-const app = express()
-const appJS = require('./app.js')
+const pokemonData = require('./pokeJSON.json');
 
-app.use(express.static('public'));
+const pokemon = ["1","1","2","2","3","3","4","4","5","5","6","6","7","7","8","8"];
 
+let shuf_pokemon = pokemon.sort(() => Math.random() - 0.5);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html')
-})
+let flippedCards = [];
+let matchedPairs = 0;
 
-app.get('/about', (req, res) => {
-  res.sendFile(__dirname + '/public/style.css')
-})
+function createPokemonBox(pokemonId) {
+  const data = pokemonData[pokemonId - 1];
+  const box = document.createElement("div");
+  box.className = "item";
+  box.dataset.id = pokemonId;
+  const img = document.createElement("img");
+  img.src = data.sprites.front_default;
+  img.alt = data.name;
+  box.appendChild(img);
 
+  box.onclick = function () {
+    if (
+      flippedCards.length < 2 &&
+      !flippedCards.includes(this) &&
+      !this.classList.contains("boxOpen")
+    ) {
+      this.classList.add("boxOpen");
+      flippedCards.push(this);
 
-const PORT = 8000
-app.listen(PORT, () => {
-  console.log(`Servidor en funcionamiento en el puerto ${PORT}`)
-})
+      if (flippedCards.length === 2) {
+        const card1 = flippedCards[0].getAttribute("data-id");
+        const card2 = flippedCards[1].getAttribute("data-id");
+
+        if (card1 === card2) {
+          flippedCards = [];
+          matchedPairs++;
+
+          if (matchedPairs === shuf_pokemon.length / 2) {
+            setTimeout(() => {
+              alert("¡Has ganado!");
+            }, 500);
+          }
+        } else {
+          setTimeout(() => {
+            flippedCards.forEach((card) => card.classList.remove("boxOpen"));
+            flippedCards = [];
+          }, 1000);
+        }
+      }
+    }
+  };
+
+  document.querySelector(".game").appendChild(box);
+}
+
+async function createGame() {
+  for (let i = 0; i < shuf_pokemon.length; i++) {
+    createPokemonBox(shuf_pokemon[i]);
+  }
+}
+
+createGame();
--- server/controllers.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-
-function goContact(req, res){
-    const filePath = path.join(__dirname, '../../public/index.html')
-    res.sendFile(filePath);
-}
-
-module.exports = { goContact }
